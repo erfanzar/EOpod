81a19eb48ee9899434213188fb3f8faf2253a90e
Omead Pooladzandi <opooladz@ucla.edu>
2025-04-28 11:49:59 -0700

    removed ray delete as it causes errors

--- eopod/_eopod_cli.py
+++ eopod/_eopod_cli.py
@@ -932,7 +932,6 @@ async def kill_tpu(worker, force, pid):
 			cleanup_commands = [
 				"sudo rm -f /tmp/libtpu_lockfile",
 				"sudo rm -rf /tmp/tpu_logs",
-				"sudo rm -rf /tmp/ray",
 				"sudo rm -rf /dev/shm/huggingface_cache/",
 				"sudo rmmod tpu || true",
 				"sudo modprobe tpu || true",
