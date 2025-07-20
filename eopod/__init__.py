8d70976b85fc67a6cd7dc7c7d08436d0339de27a
erfanzar <erfanzare82@yahoo.com>
2025-07-20 15:50:49 +0330

    Update version to 0.0.22 and expose main function in __all__

--- eopod/__init__.py
+++ eopod/__init__.py
@@ -11,5 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from ._eopod_cli import main
 
-__version__ = "0.0.21"
+__all__ = ("main",)
+__version__ = "0.0.22"
